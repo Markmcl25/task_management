7c7db6da6be377635a3a30c8d34515ccc4089c5a
Mark Mcloughlin <mark25mky@gmail.com>
2024-05-26 15:27:30 +0000

    added event details js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const firstname= document.getElementById('firstname').ariaValueMax;
 const secondName = document.getElementById('secondName').value;
 const eventType = document.getElementById('eventType').value;
 const email = document.getElementById('email').value;
+
+const eventDetails = {
+    first_name: firstname,
+    second_name: secondName,
+    event_type: eventType,
+    email: email,
+};
+
