dd17e4c3c674b0126b50fc97cac443015e23548d
Mark Mcloughlin <mark25mky@gmail.com>
2024-05-26 14:49:58 +0000

    added js input fields as well as function to add new event

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // Function to handle adding a new event
 async function addEvent() {}
+// Input fields //
+const firstname= document.getElementById('firstname').ariaValueMax;
+const secondName = document.getElementById('secondName').value;
+const eventType = document.getElementById('eventType').value;
+const email = document.getElementById('email').value;
